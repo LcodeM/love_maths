e3c6b2fbd91765c02a452a8cf87b682fa6b5e7dc
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-05-18 11:56:27 +0000

    Add function content to generate random number upon running game in runGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,19 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+
+
 // Run Game Function
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
-    
+    // Creates random number to be generated when game is run
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
 }
+
 // Check Answer Function
 function checkAnswer() {
 

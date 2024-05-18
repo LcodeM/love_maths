a2889f2200a9c6350c5020a286c15124914d4071
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-05-18 09:47:19 +0000

    Add structure for javascript functions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Run Game Function
+function runGame() {
+
+}
+// Check Answer Function
+function checkAnswer() {
+
+}
+
+// Calculate Correct Answer Function
+function calculateCorrectAnswer() {
+
+}
+
+// Increment Score Function
+function incrementScore() {
+
+}
+
+// Increment Wrong Answer Function
+function incrementWrongAnswer() {
+
+}
+
+// Display Addition Question Function
+function displayAdditionQuestion() {
+
+}
+
+// Display Subtract Question Function
+function displaySubtractQuestion() {
+
+}
+
+// Display Multiply Question Function
+function displayMultiplyQuestion() {
+
+}

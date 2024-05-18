a3e2e52f2bceb72419f004c8962fb0f6c2f21eb2
Lewis Mayhew <lewis_mayhew@hotmail.co.uk>
2024-05-18 12:59:58 +0000

    Fix displayAdditionQuestion syntax for bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = this.getElementsByTagName("button");
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!")
+                alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
     runGame("addition");
-})
+});
 
 
 
@@ -61,7 +61,7 @@ function incrementWrongAnswer() {
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementbyId('operator').textContent = "+";
+    document.getElementById('operator').textContent = "+";
 }
 
 // Display Subtract Question Function
